02885a52de4eea7a9125cc0820e1c9699375ecba
Elfin Jonathan <Elfin_J0218@mitrais.com>
2019-02-12 13:56:07 +0800

    Add socket connection

--- server/server.js
+++ server/server.js
@@ -1,12 +1,24 @@
 const _express = require('express');
+const _http = require('http');
 const _path = require('path');
+const _socketIO = require('socket.io');
 
 const publicPath = _path.join(__dirname, '../public');
 const port = process.env.PORT || 3000;
 var app = _express();
+var server = _http.createServer(app);
+var io = _socketIO(server);
 
 app.use(_express.static(publicPath));
 
-app.listen(port, () => {
+io.on('connection', (socket) => {
+   console.log('New user connected'); 
+
+   socket.on('disconnect', () => {
+    console.log('User was disconnected');
+  });
+});
+
+server.listen(port, () => {
     console.log('Start up at port ', port);
 });
